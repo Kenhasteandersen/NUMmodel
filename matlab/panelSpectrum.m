f6b9861fe6f72840cb419cc06a1503b2180cc9b7
Ken H. Andersen <kha@aqua.dtu.dk>
2020-12-24 08:55:32 +0100

    Normalized the spectrum

--- matlab/panelSpectrum.m
+++ matlab/panelSpectrum.m
@@ -1,3 +1,7 @@
+%
+% Plot Sheldon biomass spectrum. The biomasses are normalised by the width
+% of the cell and multiplied by mass.
+%
 function panelSpectrum(sim)
 p = sim.p;
 ixTime = length(sim.t);
@@ -6,7 +10,7 @@ for iGroup = 1:p.nGroups
     ix = p.ixStart(iGroup):p.ixEnd(iGroup);
     m = p.m(ix);
     
-    loglog(p.m(ix), sim.B(ixTime,ix-2), 'linewidth',2)
+    loglog(p.m(ix), sim.B(ixTime,ix-2)./p.mDelta(ix).*p.m(ix), 'linewidth',2)
     hold on
 end
 ylim([0.0001,500])
@@ -14,4 +18,4 @@ xlim([1e-9, max(sim.p.m)])
 hold off
 
 xlabel('Mass ({\mu}gC)')
-ylabel('Biomass ({\mu}gC/L)')
+ylabel('Sheldon biomass ({\mu}gC/L)')
