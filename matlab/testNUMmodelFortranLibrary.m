9fcff24ff8276a189a70fa5be85c9fda02cec5e9
Ken H. Andersen <kha@aqua.dtu.dk>
2021-02-11 19:52:52 +0100

    Added calcRates

--- matlab/testNUMmodelFortranLibrary.m
+++ matlab/testNUMmodelFortranLibrary.m
@@ -1,29 +1,3 @@
-unloadlibrary(loadNUMmodelLibrary());
-loadNUMmodelLibrary();
-
-u(1) = 150.;
-u(2) = 1.;
-u(3:12) = 1:10;
-%u(13:22) = 1.;
-%u(3:22) = 1:20;
-dudt = 0*u;
-L = 150;
-%calllib(loadNUMmodelLibrary(), 'f_setupgeneric', int32(1), 0.1);
-calllib(loadNUMmodelLibrary(), 'f_setupgeneralistsonly');
-[u, dudt] = calllib(loadNUMmodelLibrary(), 'f_calcderivatives', length(u), u, L, 0.0, dudt);
-
-
-p = parameters([]);
-rates = calcDerivatives(p, u, L);
-
-format longe
-%rates.F./p.m
-
-rates.dudt
-dudt
-
-
-%%
 
 %clearvars
 
