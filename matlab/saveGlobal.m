d02c60fa1252e46a626bf48b684f9034dd79982d
Ken H. Andersen <kha@aqua.dtu.dk>
2021-02-14 14:12:00 +0100

    Initial condition

--- matlab/saveGlobal.m
+++ matlab/saveGlobal.m
@@ -3,11 +3,13 @@
 %
 function saveGlobal(sim)
 
-sim.N = sim.N(:,:,:,end);
-sim.DOC = sim.DOC(:,:,:,end);
-sim.B = sim.B(:,:,:,:,end);
-sim.t = 0;
-sim.p = sim.p;
+savesim.N = sim.N(:,:,:,end);
+savesim.DOC = sim.DOC(:,:,:,end);
+sim.B(sim.B<0) = 0;
+savesim.B = sim.B(:,:,:,:,end);
+savesim.t = 0;
+savesim.p = sim.p;
+sim = savesim;
 
 save(sim.p.pathInit,'sim');
 
