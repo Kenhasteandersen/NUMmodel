d402526ee8f6d678ab8661ceff01d9fd66a906fb
Ken Andersen <kha@aqua.dtu.dk>
2021-04-13 16:42:36 +0200

    Simplified

--- matlab/animateGlobal.m
+++ matlab/animateGlobal.m
@@ -1,35 +1,33 @@
 %
-% Create an animation of a global run. Currently plots the plankton biomass
+% Create an animation of a field from a global run. 
+% Currently plots the plankton biomass
 % and the nutrients.
 %
-function F = animateGlobal(sim, sFilename, sProjection)
+% In: 
+%  field with three dimensions: x, y, and time
+%
+%
+function F = animateGlobal(x,y,field, options)
 
 arguments
-    sim struct;
-    sFilename char = "Global";
-    sProjection char = "fast";
+    x,y (:,:) double;
+    field (:,:,:) double;
+    options.limit double = max(field(:));
+    options.sTitle char = "";
+    options.sUnits char = "Concentration (\mug N l^{-1})";
+    options.sFilename char = "Global";
+    options.sProjection char = "fast";
 end
 
-
-n = length(sim.t);
+n = size(field,3);
 F(n) = struct('cdata',[],'colormap',[]);
 
 figure(1)
-for iTime = 1:n
+parfor iTime = 1:n
     clf
-    tiledlayout(2,1)
-    nexttile
-    % Nutrients:
-    c = panelGlobal(sim.x,sim.y,sim.N(:,:,1,iTime),'N',sProjection);
-    caxis([0 350])
-    c.Label.String  = 'Concentration [\mug N l^{-1}]';
-
-    % Unicellular plankton
-    nexttile
-    panelGlobal(sim.x,sim.y,log10(sum(sim.B(:,:,1,findIxUnicellular(sim.p),iTime),4)),'Unicellular plankton (log10)',sProjection);
-    caxis([1 3])
-    ax2 = gca;
-    ax2.NextPlot = 'replace';
+    c = panelGlobal(x,y,field(:,:,iTime),options.sTitle,options.sProjection);
+    c.Label.String  = options.sUnits;
+    caxis([0 options.limit]);
 
     drawnow
     F(iTime) = getframe(figure(1));
@@ -39,9 +37,9 @@ end
 % Write the animation:
 %
 if (ismac || ispc)
-    v = VideoWriter(sFilename, 'MPEG-4');
+    v = VideoWriter(options.sFilename, 'MPEG-4');
 else
-    v = VideoWriter(sFilename, 'Motion JPEG AVI');
+    v = VideoWriter(options.sFilename, 'Motion JPEG AVI');
 end
 v.FrameRate = n/5;
 
