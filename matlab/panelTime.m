4cb88aad5cc77001e5cb2ebe8c6a15ef05e7c646
kenhasteandersen <kha@aqua.dtu.dk>
2021-04-19 19:08:47 +0200

    Fixed panelTime.m

--- matlab/panelTime.m
+++ matlab/panelTime.m
@@ -4,14 +4,15 @@ t = sim.t;
 semilogy(t, sim.N,'b-')
 hold on
 semilogy(t, sim.DOC,'color',[181 100 30]/256)
+textLegend = {'N','DOC'};
 if isfield(sim,'Si')
-    semilogy(t, sim.DOC,'color',[181 100 30]/256)
+    semilogy(t, sim.Si,'color',[181 180 0]/256)
+    textLegend{3} = 'Si';
 end
 
-textLegend = {'N','DOC'};
 for iGroup = 1:sim.p.nGroups
     semilogy(t, sim.Bgroup(:,iGroup),'k-', 'linewidth',0.5*iGroup);
-    textLegend{iGroup+2} = sprintf('%2i',iGroup);
+    textLegend{iGroup+sim.p.idxB-1} = sprintf('%2i',iGroup);
 end
 ylim([0.1, 10000])
 axis('tight')
