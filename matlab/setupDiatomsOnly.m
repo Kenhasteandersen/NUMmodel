9b3bae8275565e522cafd39e425c498b351b83c4
kenhasteandersen <kha@aqua.dtu.dk>
2021-04-20 14:51:39 +0200

    added testDiatoms

---
+++ matlab/setupDiatomsOnly.m
@@ -0,0 +1,29 @@
+function p = setupDiatomsOnly(n)
+
+arguments
+    n {mustBeInteger, mustBePositive} = 10;
+end
+
+loadNUMmodelLibrary();
+calllib(loadNUMmodelLibrary(), 'f_setupdiatomsonly', int32(n) );
+
+p.n = n+3;
+p.nGroups = 1;
+
+p.idxN = 1;
+p.idxDOC = 2;
+p.idxSi = 3;
+p.idxB = 4; % We have three nutrient groups so biomass groups starts at index 4.
+
+
+p.n = n+p.idxB-1;
+p.nGroups = 1;
+p.typeGroups = 1;
+p.ixStart = p.idxB;
+p.ixEnd = p.n;
+
+mMax = 0.1;
+[p.m, p.mLower, p.mDelta] = parametersCalcGrid(10^-8.5, 0.1, p.n);
+
+p.u0(1:3) = [150, 0, 150]; % Initial conditions (and deep layer concentrations)
+p.u0(p.idxB:p.n) = 1;
