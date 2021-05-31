64710dc4f80878b1bd195c7e1f3ec57ce0673c78
Ken H. Andersen <Kenhasteandersen>
2021-05-31 15:52:44 +0200

    fixed global plots

--- matlab/plotGlobalWatercolumnTime.m
+++ matlab/plotGlobalWatercolumnTime.m
@@ -15,11 +15,11 @@ idx = calcGlobalWatercolumn(lat,lon,sim);
 t = sim.t;
 z = sim.z(idx.z);
 
-
 if bNewPlot
     clf
-    tiledlayout(3,1,'tilespacing','compact','padding','compact')
+    tiledlayout(2+isfield(sim,'Si')+sim.p.nGroups,1,'tilespacing','compact','padding','compact')
 end
+
 nexttile
 surface(t,-z, squeeze((double(sim.N(idx.x, idx.y, idx.z,:)))))
 title(['Nitrogen, lat ', num2str(lat),', lon ', num2str(lon)])
@@ -31,6 +31,20 @@ shading interp
 axis tight
 colorbar('ticks',10.^(-2:3))
 caxis([0.1 100])
+
+if isfield(sim,'Si')
+    nexttile
+surface(t,-z, squeeze((double(sim.Si(idx.x, idx.y, idx.z,:)))))
+title(['Silicate, lat ', num2str(lat),', lon ', num2str(lon)])
+ylabel('Depth (m)')
+%set(gca,'XTickLabel','');
+%set(gca,'yscale','log')
+set(gca,'ColorScale','log')
+shading interp
+axis tight
+colorbar('ticks',10.^(-2:3))
+caxis([0.1 1000])
+end
 
 nexttile
 surface(t,-z, squeeze((double(sim.DOC(idx.x, idx.y, idx.z,:)))))
@@ -44,9 +58,11 @@ axis tight
 colorbar
 caxis([0.1,2])
 
+for i = 1:sim.p.nGroups
 nexttile
-surface(t,-z, squeeze((sum(double(sim.B(idx.x, idx.y, idx.z,:,:)),4))))
-title(['Plankton, lat ', num2str(lat),', lon ', num2str(lon)])
+surface(t,-z, squeeze((sum(double(sim.B(idx.x, idx.y, idx.z,...
+    (sim.p.ixStart(i):sim.p.ixEnd(i))-sim.p.idxB+1,:)),4))))
+title(sim.p.nameGroup(i))
 ylabel('Depth (m)')
 xlabel('Time (days)')
 %set(gca,'yscale','log')
@@ -56,5 +72,6 @@ axis tight
 colorbar
 caxis([0.1 100])
 colorbar('ticks',10.^(-2:3))
+end
 
 end
--- matlab/saveGlobal.m
+++ matlab/saveGlobal.m
@@ -10,6 +10,9 @@ end
 
 savesim.N = sim.N(:,:,:,end);
 savesim.DOC = sim.DOC(:,:,:,end);
+if isfield(sim, 'Si')
+    savesim.Si = sim.Si(:,:,:,end);
+end
 sim.B(sim.B<0) = 0;
 savesim.B = sim.B(:,:,:,:,end);
 savesim.t = 0;
