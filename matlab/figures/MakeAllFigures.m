11d48a0da440f5b3754f749fdf9e83f070769256
kenhasteandersen <kha@aqua.dtu.dk>
2021-09-28 16:06:00 +0200

    Made figures directory

---
+++ matlab/figures/MakeAllFigures.m
@@ -0,0 +1,16 @@
+%
+% Make all figures for the manuscript
+%
+addpath('..')
+
+%
+% Sweeping across nutrient levels
+%
+figure(1)
+figureNutrientSweep
+
+%
+% Sweep across mixing rates
+%
+figure(2)
+figureMixingRateSweep
