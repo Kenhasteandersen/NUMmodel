e02d72bfd7890a95b146207cefb1a1fe0922d710
kenhasteandersen <kha@aqua.dtu.dk>
2021-12-15 20:26:28 +0100

    Fixed groups

--- matlab/plotGlobalGroups.m
+++ matlab/plotGlobalGroups.m
@@ -15,31 +15,21 @@ arguments
     options.bAverageTime = false;
 end
 
-
 clf
 tiledlayout(sim.p.nGroups,1)
 
 for i = 1:sim.p.nGroups
     nexttile
-    
+    % Extract the group:
     ix = (sim.p.ixStart(i):sim.p.ixEnd(i)) -sim.p.idxB+1;
-    %if options.bAverageTime
-    %    B = mean(sim.B(:,:,:,ix,:),5);
-    %else
-    %    B = squeeze(sim.B(:,:,:,ix,iTime));
-    %end
-    %B(isnan(B)) = 0;
-    %B = sum(B,4); % Total biomass in the group
-    % Integrate over depth:
-    %dz = sim.dznom;
-    %B = sum(B.*reshape(dz ,1,1,numel(dz)),3) / 1000; % g/m2 
-    
-    B = calcIntegrateGlobal(sim, sim.B, options.bAverageTime);
+    B = calcIntegrateGlobal(sim, sim.B(:,:,:,ix,:), options.bAverageTime);
+    % Average over time:
     if ~options.bAverageTime
         B = B(:,:,iTime);
     end
-    
-    c = panelGlobal(sim.x,sim.y, log10(B),[-1 2],sTitle=sim.p.nameGroup{i},...
+    % Plot the group:
+    c = panelGlobal(sim.x,sim.y, log10(B),[-1 2], ...
+        sTitle=sim.p.nameGroup{i},...
         sProjection=options.sProjection);
     c.Label.String  = 'log10(g/m^2)';
 end
