19381339d0497cd5f743d131429bf79431e3bfe9
kenhasteandersen <kha@aqua.dtu.dk>
2021-12-15 07:58:25 +0100

    Added calcIntegrateGlobal

--- matlab/plotGlobalGroups.m
+++ matlab/plotGlobalGroups.m
@@ -23,16 +23,21 @@ for i = 1:sim.p.nGroups
     nexttile
     
     ix = (sim.p.ixStart(i):sim.p.ixEnd(i)) -sim.p.idxB+1;
-    if options.bAverageTime
-        B = mean(sim.B(:,:,:,ix,:),5);
-    else
-        B = squeeze(sim.B(:,:,:,ix,iTime));
-    end
-    B(isnan(B)) = 0;
-    B = sum(B,4); % Total biomass in the group
+    %if options.bAverageTime
+    %    B = mean(sim.B(:,:,:,ix,:),5);
+    %else
+    %    B = squeeze(sim.B(:,:,:,ix,iTime));
+    %end
+    %B(isnan(B)) = 0;
+    %B = sum(B,4); % Total biomass in the group
     % Integrate over depth:
-    dz = sim.dznom;
-    B = sum(B.*reshape(dz ,1,1,numel(dz)),3) / 1000; % g/m2 
+    %dz = sim.dznom;
+    %B = sum(B.*reshape(dz ,1,1,numel(dz)),3) / 1000; % g/m2 
+    
+    B = calcIntegrateGlobal(sim, sim.B, options.bAverageTime);
+    if ~options.bAverageTime
+        B = B(:,:,iTime);
+    end
     
     c = panelGlobal(sim.x,sim.y, log10(B),[-1 2],sTitle=sim.p.nameGroup{i},...
         sProjection=options.sProjection);
