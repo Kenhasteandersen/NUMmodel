74242a3c7d3c7da9bb1792346f01abd32183f776
kenhasteandersen <kha@aqua.dtu.dk>
2022-01-02 17:04:20 +0100

    Introduced temperature in diatoms_simple

--- matlab/testAll.m
+++ matlab/testAll.m
@@ -1,8 +1,8 @@
 testFunction('testLoadFortran');
 
-testSetup('setupGeneralistsOnly', 2397);
-testSetup('setupGeneralistsDiatoms_simple', 1251);
-testSetup('setupGeneralistsDiatoms', 6033);
+testSetup('setupGeneralistsOnly', 2399);
+testSetup('setupGeneralistsDiatoms_simple', 1252);
+testSetup('setupGeneralistsDiatoms', 6034);
 testSetup('setupDiatoms_simpleOnly', -1170);
 testSetup('setupGeneralists_cspOnly', 1366);
 testSetup('setupGeneric', 2321);
