050a01e11fa383bd8c2c0f1d917baa5965a6b5b6
kenhasteandersen <kha@aqua.dtu.dk>
2022-04-01 15:35:55 +0200

    Changed backslashes to slashes

--- Fortran/input.f90
+++ Fortran/input.f90
@@ -7,7 +7,7 @@ module input
   
   subroutine open_inputfile(file_unit, io_err)
     integer,  intent(out) :: file_unit, io_err
-    open(newunit=file_unit,action='read', file="..\input\input.nlm",iostat=io_err)
+    open(newunit=file_unit,action='read', file="../input/input.nlm",iostat=io_err)
     call check_iostat(io_err, &
         "Could not open file 'input.nml', perhaps it does not exist?")
   end subroutine open_inputfile
