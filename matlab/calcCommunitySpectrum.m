cba25e60053945cdd2b048284f5cbb921fe78dc7
Thanos Kandylas <55313245+ThanosKand@users.noreply.github.com>
2022-04-07 14:59:57 +0200

    Calculate/plot Community Spectrum w/ Interpolation

--- matlab/calcCommunitySpectrum.m
+++ matlab/calcCommunitySpectrum.m
@@ -1,34 +1,27 @@
 %
 % Calculate the community size spectrum from all groups.
 %
-% In:
-%  m - mass of all biomass groups
-%  B - biomasses
-%
-% Out:
-%  mc - masses of the community spectrum (sorted version of m)
-%  Bc - Sheldon community spectrum
-%
-function [m, Bc] = calcCommunitySpectrum(p,B)
 
-[m, idx] = sort(p.m(3:end));
-Bcumm = cumsum(B(idx));
-Bc = gradient(Bcumm);
+function Bc = calcCommunitySpectrum(sim, mc)
 
-
-% m = logspace(min(log10(p.mLower(p.idxB:end))), max(log10(p.mLower(p.idxB:end)+p.mDelta(p.idxB:end))), 100);
-% Bspline = 0*m;
-% B = B./log(p.mUpper(p.idxB:end)./p.mLower(p.idxB:end));
-% for i = 1:p.nGroups
-%     ix = p.ixStart(i):p.ixEnd(i);
-%     ix = ix( B(ix-p.idxB+1)>0 );
-%     
-%     ixSpline = ( m>p.m(p.ixStart(i)) & m<(p.m(p.ixEnd(i))) );
-% 
-%     logBsplineGroup = spline(log10(p.m(ix)), log10(B(ix-p.idxB+1)), log10(m(ixSpline)));
-%     Bspline(ixSpline) = Bspline(ixSpline) + 10.^logBsplineGroup;
-%     loglog(m(ixSpline), 10.^logBsplineGroup)
-%     hold on
-% end
-% 
-
+    p = sim.p;
+    nPoints = length(mc);
+    Bc = zeros(1, nPoints);
+    
+    for iGroup = 1:p.nGroups
+    
+        ix = p.ixStart(iGroup):p.ixEnd(iGroup);
+        m = p.m(ix);
+        Delta = p.mUpper(ix)./p.mLower(ix);
+        ixB = ix-p.idxB+1;
+        ixAve = find( sim.t > sim.t(end)/2 );
+    
+        % Interpolation
+        log_k = mean( log(sim.B(ixAve, ixB)./log(Delta)),1);
+        vq1 = exp(interp1(log(m), log_k, log(mc), 'linear'));
+    
+        vq1(isnan(vq1)) = 0; % get rid of the NAs
+        Bc = Bc + vq1;
+    end
+    
+    end    
--- matlab/panelSpectrum.m
+++ matlab/panelSpectrum.m
@@ -4,65 +4,67 @@
 %
 function panelSpectrum(sim, ixTime)
 
-arguments
-    sim struct;
-    ixTime {mustBeInteger} = length(sim.t); % Defaults to last time step
-end
-p = sim.p;
-
-sLegend = {};
-
-for iGroup = 1:p.nGroups
-    ix = p.ixStart(iGroup):p.ixEnd(iGroup);
-    m = p.m(ix);
-    Delta = p.mUpper(ix)./p.mLower(ix);
-    ixB = ix-p.idxB+1;
-    %
-    % Plot background to show oscillations over the last half of the simulation:
-    %
-    ixAve = find( sim.t > sim.t(end)/2 );
-    if (length(sim.t) > 1)
-        Blower = min( sim.B(ixAve,ixB) )./ log(Delta);
-        Bupper = max( sim.B(ixAve,ixB) )./ log(Delta);
-        patch([m, m(end:-1:1)], [Blower, Bupper(end:-1:1)] , ...
-            p.colGroup{iGroup},...
-            'edgecolor','none', 'facealpha',0.15);
-        [mc, Bc] = calcCommunitySpectrum(sim.p, exp(mean(log(sim.B(ixAve,:)),1)));
-    else
-        [mc, Bc] = calcCommunitySpectrum(sim.p, sim.B);
+    arguments
+        sim struct;
+        ixTime {mustBeInteger} = length(sim.t); % Defaults to last time step
     end
-    set(gca,'xscale','log','yscale','log')
-    hold on
-end
-%
-% Community spectrum:
-%
-
-loglog(mc, Bc, 'linewidth',2,'color',[0.7, 0.7, 0.7])
-%
-% Group spectra:
-%
-for iGroup = 1:p.nGroups
-    ix = p.ixStart(iGroup):p.ixEnd(iGroup);
-    m = p.m(ix);
-    Delta = p.mUpper(ix)./p.mLower(ix);
-    ixB = ix-p.idxB+1;
+    p = sim.p;
+    
+    sLegend = {};
+    
+    for iGroup = 1:p.nGroups
+        ix = p.ixStart(iGroup):p.ixEnd(iGroup);
+        m = p.m(ix);
+        Delta = p.mUpper(ix)./p.mLower(ix);
+        ixB = ix-p.idxB+1;
+        %
+        % Plot background to show oscillations over the last half of the simulation:
+        %
+        ixAve = find( sim.t > sim.t(end)/2 );
+        if (length(sim.t) >1)
+            Blower = min( sim.B(ixAve,ixB) )./ log(Delta);
+            Bupper = max( sim.B(ixAve,ixB) )./ log(Delta);
+            patch([m, m(end:-1:1)], [Blower, Bupper(end:-1:1)] , ...
+                p.colGroup{iGroup},...
+                'edgecolor','none', 'facealpha',0.15);
+        end
+        set(gca,'xscale','log','yscale','log')
+        hold on
+    end
     %
-    % Plot the spectrum:
+    % Community spectrum:
     %
-    legendentries(iGroup) = ...
-        loglog(m, exp( mean( log(sim.B(ixAve, ixB)./log(Delta)),1)), 'linewidth',2,...
-        'color',p.colGroup{iGroup});
-    loglog(m, sim.B(ixTime, ixB)./log(Delta), ':','linewidth',1,...
-        'color',p.colGroup{iGroup})
-
-    sLegend{iGroup} = p.nameGroup{iGroup};
-end
-ylim([0.1,500])
-xlim(calcXlim(sim.p))
-hold off
-
-xlabel('Mass ({\mu}gC)')
-ylabel('Sheldon biomass ({\mu}gC/L)')
-
-legend(legendentries, sLegend, 'location','northeast','box','off')
+    nPoints = 100000;
+    mc = logspace(log10(p.m(3)), log10(p.m(end)), nPoints);
+    
+    Bc = calcCommunitySpectrum(sim,mc);
+    legendentries(1) = loglog(mc, Bc, 'linewidth', 4.5,'color',[0.7, 0.7, 0.7]);
+    sLegend{1} = 'Community spectrum';
+    
+    %
+    % Group spectra:
+    %
+    for iGroup = 1:p.nGroups
+        ix = p.ixStart(iGroup):p.ixEnd(iGroup);
+        m = p.m(ix);
+        Delta = p.mUpper(ix)./p.mLower(ix);
+        ixB = ix-p.idxB+1;
+        %
+        % Plot the spectrum:
+        %
+        legendentries(iGroup+1) = ...
+            loglog(m, exp( mean( log(sim.B(ixAve, ixB)./log(Delta)),1)), 'linewidth',2,...
+            'color',p.colGroup{iGroup});
+        loglog(m, sim.B(ixTime, ixB)./log(Delta), ':','linewidth',1,...
+            'color',p.colGroup{iGroup})
+    
+        sLegend{iGroup+1} = p.nameGroup{iGroup};
+    end
+    ylim([0.1,500])
+    xlim(calcXlim(sim.p))
+    hold off
+    
+    xlabel('Mass ({\mu}gC)')
+    ylabel('Sheldon biomass ({\mu}gC/L)')
+    
+    legend(legendentries, sLegend, 'location','northeast','box','off')
