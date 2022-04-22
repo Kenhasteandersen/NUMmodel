d2e50618b7d2b4789be7580072ada4f98fa1e353
kenhasteandersen <kha@aqua.dtu.dk>
2022-04-22 13:31:11 +0200

    Lowered default mixing rate to 0.1

--- matlab/parametersChemostat.m
+++ matlab/parametersChemostat.m
@@ -11,7 +11,7 @@ function p = parametersChemostat(p)
 
 p.nameModel = 'chemostat';
 
-p.d = 0.5;  % Mixing rate (1/days)
+p.d = 0.1;  % Mixing rate (1/days)
 p.tEnd = 365;  % Time to run in days
 p.tSave = 1;
 
