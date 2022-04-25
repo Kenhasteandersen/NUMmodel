e8d813c544d2c525c159fbb42934415e61712946
Thanos Kandylas <55313245+ThanosKand@users.noreply.github.com>
2022-04-25 21:08:50 +0200

    Update baserunWatercolumn.m

--- matlab/baserunWatercolumn.m
+++ matlab/baserunWatercolumn.m
@@ -12,11 +12,22 @@ function sim = baserunWatercolumn(lat,lon)
 arguments
     lat double = 60;
     lon double = -10;
+
+%     lat double = 0;
+%     lon double = -23;
+
 end
 
-p = setupGeneralistsOnly(25);
+mAdults = logspace(log10(0.2), log10(1000), 10);
+
+
+% p = setupGeneralistsOnly(25);
+p = setupGeneric(mAdults);
 p = parametersWatercolumn(p);
-p.tEnd = 2*365;
+% p.tEnd = 2*365;
+p.tEnd = 1000;
+
+setHTL(0.1, 10, true, true);
 
 sim = simulateWatercolumn(p, lat,lon);
 
