76f5957c6f765d365da798830ad2a87cb20cff0c
Thanos Kandylas <55313245+ThanosKand@users.noreply.github.com>
2022-05-05 12:42:26 +0200

    Run.bat file modification
    Depending on the operating system, these modifications must be done:
    
    rm -> del
    mv -> move

--- Fortran/run.bat
+++ Fortran/run.bat
@@ -1,6 +1,6 @@
-rm ..\lib\libNUMmodel_matlab.dll
+del ..\lib\libNUMmodel_matlab.dll
 make clean
-rm *.mod
+del *.mod
 gfortran -c -fPIC input.f90
 gfortran -c -fPIC globals.f90
 gfortran -c -fPIC spectrum.f90
@@ -8,4 +8,4 @@ gfortran -c -fPIC POM.f90
 gfortran -c -fPIC *.f90
 
 gfortran -shared *o -o libNUMmodel_matlab.dll
-mv libNUMmodel_matlab.dll ../lib/.
+move libNUMmodel_matlab.dll ../lib/.
