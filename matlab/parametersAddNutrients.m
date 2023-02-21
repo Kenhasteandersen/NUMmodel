dd19c3695b0966641b4226d878e4de579fb9dcb3
kenhasteandersen <kha@aqua.dtu.dk>
2023-02-21 15:35:57 +0100

    Updates to runBifurcation

--- matlab/parametersAddNutrients.m
+++ matlab/parametersAddNutrients.m
@@ -14,3 +14,16 @@ p.nameNutrientsLong{p.nNutrients} = nameLong;
 p.nameNutrientsUnits{p.nNutrients} = nameUnits;
 p.(sprintf('idx%s',nameShort)) = p.nNutrients;
 p.n = p.nNutrients;
+
+if strcmp(p.nameNutrientsShort, 'N') 
+    p.colNutrients{p.nNutrients} = [0 0 1];
+end
+
+if strcmp(p.nameNutrientsShort, 'DOC') 
+    p.colNutrients{p.nNutrients} = [1 0 1];
+end
+
+if strcmp(p.nameNutrientsShort, 'Si') 
+    p.colNutrients{p.nNutrients} = [0 1 1];
+end
+
