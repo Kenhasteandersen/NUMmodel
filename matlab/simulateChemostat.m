d888d40e2a4d4f326294fe7d6bcf6541d1993593
kenhasteandersen <kha@aqua.dtu.dk>
2023-02-23 12:25:22 +0100

    fixed exchanged of diatoms to deep layer

--- matlab/simulateChemostat.m
+++ matlab/simulateChemostat.m
@@ -37,7 +37,7 @@ sim.L = L;
 % Concentrations in the deep layer:
 %
 if options.bUnicellularloss
-    ix = 1:p.ixEnd(1); % Nutrients and first field (unicellulars) are lost to the deep layer
+    ix = 1:p.ixEnd(max(find(p.typeGroups<10))); % Nutrients and unicellulars are lost to the deep layer
 else
     ix = 1:(p.idxB-1); % Nutrients
 end
