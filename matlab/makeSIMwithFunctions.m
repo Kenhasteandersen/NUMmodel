8dacb16cbb63c4c8e08fbb25648c80c53da15c32
kenhasteandersen <kha@aqua.dtu.dk>
2023-05-11 12:07:48 +0200

    removed superfluous files

--- matlab/makeSIMwithFunctions.m
+++
@@ -1,5 +0,0 @@
-% load('C:\Users\ampap\OneDrive - Danmarks Tekniske Universitet\Model EVALUATION\simGeneralists.mat')
-load('C:\Users\ampap\OneDrive - Danmarks Tekniske Universitet\Model EVALUATION\sim5000.mat')
-sim=sim5000;
-% sim=simGeneralists;
-sim = calcFunctions(sim);
