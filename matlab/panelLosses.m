60a98632a4aed15ca2aa490a829722c201d7683b
kenhasteandersen <kha@aqua.dtu.dk>
2023-06-05 10:56:44 +0200

    Fixed line types for diatoms

--- matlab/panelLosses.m
+++ matlab/panelLosses.m
@@ -1,16 +1,25 @@
 function panelLosses(p,rates)
 
+
+
 for iGroup = 1:p.nGroups
+    if ((p.typeGroups(iGroup)==3) || (p.typeGroups(iGroup)==4))
+        sLinetype = "--"; % For diatoms
+    else
+        sLinetype = "-"; % For all others
+    end
+
     ix = (p.ixStart(iGroup):p.ixEnd(iGroup))-p.idxB+1;
     m = p.m(ix+p.idxB-1);
-    semilogx(m, rates.mortpred(ix), 'r-','linewidth',2)
+
+    semilogx(m, rates.mortpred(ix), 'r','linewidth',2, 'linestyle',sLinetype)
     hold on
     %semilogx(m, rates.jR(ix), 'k-.', 'linewidth',2)
-    semilogx(m, rates.jRespTot(ix), 'k-', 'linewidth',2)
-    semilogx(m, rates.mort2(ix), 'b-','linewidth',2)
-    semilogx(m, rates.jLossPassive(ix), 'color',[0 0.5 0],'linewidth',2)
-%    loglog(m, rates.mortStarve(ix), 'b-o','linewidth',2)
-    loglog(m, rates.mortHTL(ix), 'm-','linewidth',2)
+    semilogx(m, rates.jRespTot(ix), 'k', 'linewidth',2, 'linestyle',sLinetype)
+    semilogx(m, rates.mort2(ix), 'b','linewidth',2, 'linestyle',sLinetype)
+    semilogx(m, rates.jLossPassive(ix), 'color',[0 0.5 0],'linewidth',2, 'linestyle',sLinetype)
+    %    loglog(m, rates.mortStarve(ix), 'b-o','linewidth',2)
+    loglog(m, rates.mortHTL(ix), 'm','linewidth',2, 'linestyle',sLinetype)
 end
 %
 % Show losses from chemostat
