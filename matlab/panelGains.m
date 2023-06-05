f84c22d2d482adcbb3d24ddfcd80ee5997184e92
kenhasteandersen <kha@aqua.dtu.dk>
2023-06-05 10:53:43 +0200

    Fixed limites when growth is negative

--- matlab/panelGains.m
+++ matlab/panelGains.m
@@ -3,31 +3,40 @@ function panelGains(p,rates)
 for iGroup = 1:p.nGroups
     ix = (p.ixStart(iGroup):p.ixEnd(iGroup))-p.idxB+1;
     m = p.m(ix+p.idxB-1);
-    semilogx(m, rates.jF(ix), 'r-', 'linewidth',2)
-    hold on
-    % Rates for unicellulars:
+    %
+    % Generalists and copepods:
+    %
     if ((p.typeGroups(iGroup)~=3) && (p.typeGroups(iGroup)~=4))
+        semilogx(m, rates.jF(ix), 'r-', 'linewidth',2)
+        hold on
        semilogx(m, rates.jN(ix), 'b-','linewidth',2)
        semilogx(m, rates.jLreal(ix), 'g-','linewidth',2)
        semilogx(m, rates.jDOC(ix), 'color',[181 100 30]/256,'linewidth',2)
        semilogx(m, rates.jMax(ix), 'k:')       
            % Total:
     semilogx(m, rates.jTot(ix), 'k-', 'linewidth',2)
+    
+    end
+    %
+    % Diatoms:
+    %
+    if ((p.typeGroups(iGroup)==3) || (p.typeGroups(iGroup)==4))
+        semilogx(m, rates.jSi(ix), 'color',[181 180 0]/256,'linewidth',2)
+        hold on
+        semilogx(m, rates.jN(ix), 'b--','linewidth',2)
+        semilogx(m, rates.jLreal(ix), 'g--','linewidth',2)
+        semilogx(m, rates.jDOC(ix), 'color',[181 100 30]/256,'linewidth',2)
+        semilogx(m, rates.jMax(ix), 'k:')
+        semilogx(m, rates.jTot(ix), 'k--', 'linewidth',2)
+    end
+    %
     % Rates for copepods:
-    %if (p.typeGroups(iGroup)~=2)
+    %
+    %if (p.typeGroups(iGroup) >= 10)
+    %    semilogx(m, rates.jF(ix), 'r-', 'linewidth',2)
+    %    hold on
     %    semilogx(m, p.jFmax(ix),'k:')
     %end
-    end
-    
-    if ((p.typeGroups(iGroup)==3) || (p.typeGroups(iGroup)==4))
-       semilogx(m, rates.jSi(ix), 'color',[181 180 0]/256,'linewidth',2)
-       semilogx(m, rates.jN(ix), 'b--','linewidth',2)
-       semilogx(m, rates.jLreal(ix), 'g--','linewidth',2)
-       semilogx(m, rates.jDOC(ix), 'color',[181 100 30]/256,'linewidth',2)
-       semilogx(m, rates.jMax(ix), 'k:') 
-       semilogx(m, rates.jTot(ix), 'k--', 'linewidth',2)
-    end
-    
 
 end
 
