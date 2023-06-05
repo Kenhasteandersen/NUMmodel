940b715ea588f70aa85fbc54644157e5d14c9692
kenhasteandersen <kha@aqua.dtu.dk>
2023-06-05 10:18:24 +0200

    Fixed limites when growth is negative

--- matlab/panelGains.m
+++ matlab/panelGains.m
@@ -6,7 +6,7 @@ for iGroup = 1:p.nGroups
     semilogx(m, rates.jF(ix), 'r-', 'linewidth',2)
     hold on
     % Rates for unicellulars:
-    if ((p.typeGroups(iGroup)~=3) || (p.typeGroups(iGroup)~=4))
+    if ((p.typeGroups(iGroup)~=3) && (p.typeGroups(iGroup)~=4))
        semilogx(m, rates.jN(ix), 'b-','linewidth',2)
        semilogx(m, rates.jLreal(ix), 'g-','linewidth',2)
        semilogx(m, rates.jDOC(ix), 'color',[181 100 30]/256,'linewidth',2)
