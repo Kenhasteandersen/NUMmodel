71ab406cb7aeffc2d25cf559542427ca9dbcb8db
kenhasteandersen <kha@aqua.dtu.dk>
2023-06-20 13:59:16 +0200

    increased sinking velocity to 20

--- matlab/setupNUMmodel.m
+++ matlab/setupNUMmodel.m
@@ -73,7 +73,7 @@ end
 
 % POM:
 p = parametersAddgroup(100, p, nPOM);
-setSinkingPOM(p, 10); % Since there is only one group, set a slow sinking
+setSinkingPOM(p, 20); % Since there is only one group, set a slow sinking
 
 p = getMass(p);
 
