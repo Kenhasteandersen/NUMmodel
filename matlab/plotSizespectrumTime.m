cbbedc6e37f15268b191114040ac2f4b07c4a65d
kenhasteandersen <kha@aqua.dtu.dk>
2023-12-07 17:20:17 +0100

    Minor updates to plotSimulation for global

--- matlab/plotSizespectrumTime.m
+++ matlab/plotSizespectrumTime.m
@@ -18,7 +18,7 @@ switch sim.p.nameModel
     case 'watercolumn'
         % Extract from a single water column:
         s.B = squeeze(sim.B(:,iDepth,:));
-        sTitle = sprintf("Community sheldon spectrum at depth of max biomass: %3.0f m (log_{10} {\\mu}g_C/l)", sim.z(iDepth));
+        sTitle = sprintf("Community sheldon spectrum at depth %3.0f m", sim.z(iDepth));
 
     case 'global'
         if isempty(lat)
@@ -28,7 +28,7 @@ switch sim.p.nameModel
             % Extract from global run:
             idx = calcGlobalWatercolumn(lat,lon,sim);
             s.B = squeeze(sim.B(:, idx.x, idx.y, iDepth, :));
-            sTitle = sprintf("Sheldon spectrum at %3.0f m (log_{10} ug_N/l)", sim.z(iDepth));
+            sTitle = sprintf("Sheldon spectrum at (%i,%i) and %3.0f m depth", [lat,lon,sim.z(iDepth)]);
         end
 
     otherwise
@@ -36,31 +36,19 @@ switch sim.p.nameModel
         stop
 end
 
-% Create Sheldon size spectrum from biomasses by dividing with bin width:
+% Create Sheldon size spectrum
 for iTime = 1:length(sim.t)
-
     [mc, s.Bc(:,iTime)] = calcCommunitySpectrum(s.B, sim, iTime);
-
-    % [mc, s.Bc(:,iTime)] = calcCommunitySpectrum(sim.p, s.B(:,iTime));
-    %s.B(:,iTime) = s.B(:,iTime) ./ sim.p.mDelta(sim.p.idxB:end)' .* m';
 end
 
 s.Bc(imag(s.Bc) ~=0) = 0.01;
 s.Bc(s.Bc < 0.01) = 0.01;
 
 clf
-% surface(mc, sim.t, log10(s.Bc)')
-
-% s.Bc( s.Bc < 0.01 ) = 0.01;
-% BB(imag(BB) ~= 0) = 0.0001;
-
 contourf( mc, sim.t, s.Bc', ...
     10.^linspace(-2,2,100),'linestyle','none')
 
-% surface(mc, sim.t, s.Bc')
-% shading flat
 axis tight
-% caxis([-2,2])
 set(gca,'xscale','log')
 set(gca, 'colorscale','log')
 
