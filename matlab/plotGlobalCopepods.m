08fe6e7414f8de4d1ae40e787de36e401cefe770
kenhasteandersen <kha@aqua.dtu.dk>
2024-02-15 11:31:27 +0100

    Added calcBiomassGroup.m

--- matlab/plotGlobalCopepods.m
+++ matlab/plotGlobalCopepods.m
@@ -6,31 +6,6 @@ arguments
     sim struct;
     options.sProjection string = 'fast';
 end
-p = sim.p;
-%%
-% Find active and passive copepods:
-%
-%Bcopepod = zeros(length(sim.t),128,64,15);
-%Bgroup = zeros(2,128,64);
-%typeGroup = 10:11;
-%for j = 1:2
-%    ixGroup = find(p.typeGroups==typeGroup(j));
-%    for i = 1:length(ixGroup)
-%        ix = (p.ixStart(ixGroup(i)):p.ixEnd(ixGroup(i))) - p.idxB+1;
-%        Bcopepod = Bcopepod + sum( sim.B(:,:,:,:,ix),5 );
-%    end
-%    Bgroup(j,:,:) = calcIntegrateGlobal(sim,Bcopepod,true);
-%end
-%%
-% Find geometric mean size:
-%
-% ixGroup = find(p.typeGroups==10 || p.typeGroups==11);
-% ixStart = min( p.ixStart(ixGroup) ) - p.idxB+1;
-% ixEnd = max ( p.ixEnd(ixGroup) ) - p.idxB+1;
-% meanTime = mean(sim.B,1);
-% for i = 1:length(sim.x)
-%     for j = 1:length(sim.y)
-%         for k = 1:length(sim.z)
             
 Bactive = calcBiomassGroup(sim,11);
 Bpassive = calcBiomassGroup(sim,10);
