c4f3577f8d76507252817e7118725e4361c354af
kenhasteandersen <kha@aqua.dtu.dk>
2024-02-15 11:30:41 +0100

    Added calcBiomassGroup.m

--- matlab/plotGlobalCopepods.m
+++ matlab/plotGlobalCopepods.m
@@ -10,17 +10,17 @@ p = sim.p;
 %%
 % Find active and passive copepods:
 %
-Bcopepod = zeros(length(sim.t),128,64,15);
-Bgroup = zeros(2,128,64);
-typeGroup = 10:11;
-for j = 1:2
-    ixGroup = find(p.typeGroups==typeGroup(j));
-    for i = 1:length(ixGroup)
-        ix = (p.ixStart(ixGroup(i)):p.ixEnd(ixGroup(i))) - p.idxB+1;
-        Bcopepod = Bcopepod + sum( sim.B(:,:,:,:,ix),5 );
-    end
-    Bgroup(j,:,:) = calcIntegrateGlobal(sim,Bcopepod,true);
-end
+%Bcopepod = zeros(length(sim.t),128,64,15);
+%Bgroup = zeros(2,128,64);
+%typeGroup = 10:11;
+%for j = 1:2
+%    ixGroup = find(p.typeGroups==typeGroup(j));
+%    for i = 1:length(ixGroup)
+%        ix = (p.ixStart(ixGroup(i)):p.ixEnd(ixGroup(i))) - p.idxB+1;
+%        Bcopepod = Bcopepod + sum( sim.B(:,:,:,:,ix),5 );
+%    end
+%    Bgroup(j,:,:) = calcIntegrateGlobal(sim,Bcopepod,true);
+%end
 %%
 % Find geometric mean size:
 %
@@ -32,6 +32,8 @@ end
 %     for j = 1:length(sim.y)
 %         for k = 1:length(sim.z)
             
+Bactive = calcBiomassGroup(sim,11);
+Bpassive = calcBiomassGroup(sim,10);
 
 %%
 % plot
@@ -41,13 +43,13 @@ tiledlayout(2,1)
 
 % Total copepod biomass:
 nexttile
-handle = panelGlobal(sim.x,sim.y,log10(squeeze(sum(Bgroup,1))),[-2 2],sProjection=options.sProjection,...
+handle = panelGlobal(sim.x,sim.y,log10(Bactive+Bpassive),[-2 2],sProjection=options.sProjection,...
     sUnits='g_C/m^2');
 
 
 % Active/passive:
 nexttile
-panelGlobal(sim.x, sim.y, Bgroup(2,:,:)./(Bgroup(1,:,:)+Bgroup(2,:,:)),[0 1],sProjection=options.sProjection,...
+panelGlobal(sim.x, sim.y, Bactive./(Bactive+Bpassive),[0 1],sProjection=options.sProjection,...
     sUnits='Fraction active')
 
 
