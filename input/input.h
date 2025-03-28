cba9d034e25ef6f821b89783b2b40bfe2f4b908b
kenhasteandersen <kha@aqua.dtu.dk>
2025-03-28 13:18:10 +0100

    Diatom p=0.5

--- input/input.h
+++ input/input.h
@@ -190,7 +190,7 @@
 
 ! Vulnerability to predation:
 !----------------------------
-  palatability = 0.1
+  palatability = 0.5
   
 !***********************************************************************
 ! COPEPODS ACTIVE INPUT PARAMETERS
