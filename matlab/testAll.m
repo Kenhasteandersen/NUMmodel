e21ddc9183af27a29d7b0bfb4d68ae4ac267ba18
kenhasteandersen <kha@aqua.dtu.dk>
2025-10-04 08:46:46 +0200

    Updated test scripts

--- matlab/testAll.m
+++ matlab/testAll.m
@@ -4,14 +4,14 @@ loadNUMmodelLibrary
 testSetup('setupGeneralistsSimpleOnly',  4170);
 testSetup('setupGeneralistsOnly',  3727);
 %testSetup('setupGeneralistsDiatoms_simple', 5487);
-testSetup('setupGeneralistsDiatoms', 11396);
+testSetup('setupGeneralistsDiatoms', 11354);
 %testSetup('setupDiatoms_simpleOnly', 1284);
 testSetup('setupDiatomsOnly', 7349);
 testSetup('setupGeneric', 4547);
-testSetup('setupNUMmodel', 1629);
+testSetup('setupNUMmodel', 1624);
 
 testFunction('testChemostat',26754);
 testFunction('testChemostatEuler',1);
 %testFunction('testChemostatSeasonal',NaN);
-testFunction('testWatercolumn',249733);
-testFunction('testGlobal',3766688);
+testFunction('testWatercolumn',189315);
+testFunction('testGlobal',3071461);
