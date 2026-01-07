c7cef06dede13586b9cbec6d390676e8ae278234
Ken H Andersen <khand@dtu.dk>
2026-01-07 16:52:20 +0100

    Updated test scores

--- matlab/testAll.m
+++ matlab/testAll.m
@@ -4,11 +4,11 @@ loadNUMmodelLibrary
 testSetup('setupGeneralistsSimpleOnly',  4170);
 testSetup('setupGeneralistsOnly',  3727);
 %testSetup('setupGeneralistsDiatoms_simple', 5487);
-testSetup('setupGeneralistsDiatoms', 11354);
+testSetup('setupGeneralistsDiatoms', 11380);
 %testSetup('setupDiatoms_simpleOnly', 1284);
 testSetup('setupDiatomsOnly', 7349);
 testSetup('setupGeneric', 4547);
-testSetup('setupNUMmodel', 1624);
+testSetup('setupNUMmodel', 1630);
 
 testFunction('testChemostat',26754);
 testFunction('testChemostatEuler',1);
